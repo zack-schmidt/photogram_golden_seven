f6c80cba9e36900e4e271c970b3ab58b72afc9f2
Raghu Betina <raghu@firstdraft.com>
2017-04-24 18:05:44 -0400

    Add controller logic

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,10 @@
 class PhotosController < ApplicationController
   def show
+    p = Photo.find(params[:the_id])
+
+    @source = p.source
+    @caption = p.caption
+    
     render("photos/show.html.erb")
   end
 end
