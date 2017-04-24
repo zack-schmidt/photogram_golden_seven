68f11ea42e31e3d1db7146eb7f201d43f16e9df6
Raghu Betina <raghu@firstdraft.com>
2017-04-24 17:53:36 -0400

    Add route for individual photo details

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get("/photo_details/:the_id", { :controller => "photos", :action => "show" })
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
