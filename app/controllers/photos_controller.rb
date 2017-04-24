db452a81df197a507324fbf2f3470e6a41466f8c
Raghu Betina <raghu@firstdraft.com>
2017-04-24 18:42:54 -0400

    Add controller logic

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,10 @@
 class PhotosController < ApplicationController
   def index
+    @list_of_photos = Photo.all
+
     render("photos/index.html.erb")
   end
-  
+
   def show
     p = Photo.find(params[:the_id])
 
