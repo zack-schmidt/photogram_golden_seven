458523bf8284db61c1d936939da1f4eb69185f64
Zack Schmidt <zack.schmidt@kellogg.northwestern.edu>
2017-04-25 11:29:35 -0500

    at end of class

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,13 +1,30 @@
 class PhotosController < ApplicationController
   def index
-    @all_photos = Photo.all
+    @all_photos = Photo.order({:created_at => :desc})
     render ("photos/index.html.erb")
+  end
+
+  def new_form
+    render("photos/new.html.erb")
+  end
+
+  def create_row
+    p = Photo.new
+    p.source = params["the_image_url"]
+    p.caption = params["the_caption"]
+    p.save
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    @the_photo_to_be_edited = Photo.find(params["the_id_number"])
+    render("photos/edit_form.html.erb")
   end
 
   def show
     # the params hash looks like this: {"the_id_number" => 1}
 
-    p = Photo.find(params["the_id_number"])
+    p = Photo.all.find(params["the_id_number"])
 
     @the_caption = p.caption
     @the_image_url = p.source
@@ -15,4 +32,13 @@ class PhotosController < ApplicationController
 
     render("show.html.erb")
   end
+
+def update_row
+  p = Photo.find(params["the_id_number"])
+  p.source = params["the_source"]
+  p.caption = params["the_caption"]
+  p.save
+  redirect_to("/photos")
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
 
+  # create
+
+  get("/photos/new", { :controller => "photos", :action => "new_form"})
+  get("/photos/create_photo", { :controller => "photos", :action => "create_row"})
+
+  #read
   get("/photos", { :controller => "photos", :action => "index"})
   get("/photos/:the_id_number", { :controller => "photos", :action => "show"})
 
+  #update
+  get("/photos/:the_id_number/edit", { :controller => "photos", :action => "edit_form"})
+  get("/update_photo/:the_id_number", { :controller => "photos", :action => "update_row"})
+
+  #delete
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
